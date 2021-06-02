3204560a9882100123a4111586ad9bb62e663ff5
Reluctant Data Scientist <deeplylearned@outlook.com>
2021-06-02 14:28:24 +0530

    added code to randomly choose n files out of entire list

--- utils/s3.py
+++ utils/s3.py
@@ -1,15 +1,24 @@
 import boto3
+import pandas as pd 
+import random 
+
+def flip(p):
+    return 1 if random.random() < p else 0
 
 def get_file_count():
-    # get the bucket
     client = boto3.client('s3') 
-    s3 = boto3.resource('s3')
+    resource = boto3.resource('s3')
     response = client.list_buckets()
     count_obj = 0
+    fileList = []
     for bucket in response['Buckets']:
-        print(bucket['Name'])
-        s3bucket = s3.Bucket(bucket['Name'])
+        s3bucket = resource.Bucket(bucket['Name'])
         for key in s3bucket.objects.all():
-            count_obj = count_obj + 1
-    return(count_obj)
+            if key.size:
+                fileList.append(bucket['Name'] + '/' +key.key)
+                count_obj = count_obj + 1
+    return(count_obj, fileList)
 
+def get_subset_files(fileList, num_files):
+    files = random.choices(fileList, k=num_files)
+    return(files)
--- utils/s3_test.py
+++ utils/s3_test.py
@@ -1,4 +1,5 @@
-import s3 
+import s3
 
+num_files, fileList = s3.get_file_count()
 
-print(s3.get_file_count())
+print(s3.get_subset_files(fileList, 10))
