5380cf1d1dabd20c2ef1e6a1020d51eed53fb5ae
Reluctant Data Scientist <deeplylearned@outlook.com>
2021-06-03 08:53:31 +0530

    added stats module

--- utils/comprehend_test.py
+++ utils/comprehend_test.py
@@ -1,9 +1,17 @@
 import comprehend
 
 client = comprehend.init()
-
-file = open('../data/output/set1/file11.txt', 'r')
+inFile = '../data/output/set1/file11.txt'
+file = open(inFile, 'r')
 text = file.read()
 
 response = comprehend.detect_ssn(client, text)
-print(response)
+print('processing file - ', inFile)
+if not response:
+    print('No SSN Found')
+for r in response:
+    if r.get('Type') == 'SSN' and r.get("Score") > 0.98:
+        print('Found SSN with Score - ', r.get("Score"))
+    else:
+        print('No SSN Found')
+    
